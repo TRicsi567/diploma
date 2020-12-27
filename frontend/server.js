67d61d5629e974299c24c04572335deec8863ab4
Richard David Török <richard.torok@lhsystems.com>
2020-12-27 10:36:07 +0100

    doc updated

--- frontend/server.js
+++ frontend/server.js
@@ -8,13 +8,21 @@ const { PORT = 3000 } = process.env;
 app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/manifest.json', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'manifest.json'));
-});
-
-app.get('/*', function (req, res) {
+app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
+
+app.get('*', (req, res) => {
+  res.redirect('/');
+});
+
+// app.get('/manifest.json', function (req, res) {
+//   res.sendFile(path.join(__dirname, 'build', 'manifest.json'));
+// });
+
+// app.get('/*', function (req, res) {
+//   res.sendFile(path.join(__dirname, 'build', 'index.html'));
+// });
 
 app.listen(PORT, () => {
   console.log(`Server running on port: ${PORT}`);
